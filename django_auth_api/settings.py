74ba2fcc87d01b79abc37afa08813a67b10e2ed8
Gopal Gautam <gopalgautam279@gmail.com>
2024-01-31 18:32:33 +0530

    some changes for k8s deployment

--- django_auth_api/settings.py
+++ django_auth_api/settings.py
@@ -94,10 +94,10 @@ AUTH_USER_MODEL = 'account.User'
 DATABASES = {
     "default": {
         'ENGINE': 'django.db.backends.postgresql',
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": 'db',
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
         "PORT": 5432,
     }
 }
